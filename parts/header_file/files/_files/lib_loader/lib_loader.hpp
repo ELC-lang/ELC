681f53e777e87129485418473eaa5840a60798e6
steve green <steve02081504@EDEN.fukwold>
2020-12-01 23:39:06 +0800

    Update lib_loader.hpp

--- parts/header_file/files/_files/lib_loader/lib_loader.hpp
+++ parts/header_file/files/_files/lib_loader/lib_loader.hpp
@@ -7,6 +7,11 @@
 项目地址：https://github.com/steve02081504/ELC
 */
 namespace lib_loader_n{
+	//UF:抽象等级太低
+	struct library_info_t;
+	typedef comn_ptr_t<library_info_t> library_handle_t;
+
+
 	struct library_info_t:ref_able<library_info_t>{
 	private:
 		library_handle _m;
@@ -14,8 +19,8 @@ namespace lib_loader_n{
 		string _error;
 	public:
 		explicit library_info_t(string lib_name)noexcept{
-			_name=move(name);
-			_m=load_library(name);
+			_name=move(lib_name);
+			_m=load_library(lib_name);
 
 			if(_m)
 				error=get_load_error();
@@ -23,19 +28,47 @@ namespace lib_loader_n{
 		~library_info_t()noexcept{
 			free_library(_m);
 		}
-		const string&get_error()noexcept{
+		[[nodiscard]]explicit operator bool()noexcept{
+			return bool(_m);
+		}
+		[[nodiscard]]const string&get_error()noexcept{
 			return _error;
 		}
-		const string&get_name()noexcept{
+		[[nodiscard]]const string&get_name()noexcept{
 			return _name;
 		}
-		template<class function_t>
-		function_t<function_t>get_symbol_as_function(){
-			
+
+		[[nodiscard]]void*get_symbol(string symbol_name)noexcept{
+			return get_symbol(_m,symbol_name);
+		}
+		template<class symbol_t>
+		[[nodiscard]]symbol_t&get_symbol_as(string symbol_name)noexcept{
+			return*reinterpret_cast<symbol_t*>(get_symbol(symbol_name));
+		}
+		template<class Func_t>
+		[[nodiscard]]function_t<Func_t>get_symbol_as_function(string symbol_name)noexcept{
+			auto func_p=&get_symbol_as<Func_t>(symbol_name);
+
+			//出错检查：返回default的function_t（可正常调用）
+			if(!func_p)return nullptr;
+
+			/*
+			call_t
+			继承function_data_warpper_t的operator()以可转换为function_t
+			容纳一个library_handle_t以引用计数lib资源
+			*/
+			struct call_t:function_data_warpper_t<Func_t>{
+				typedef function_data_warpper_t<Func_t>base_t;
+				library_handle_t _m;
+				call_t(library_info_t*a,Func_t*b)noexcept:_m(a),base_t(b){}
+				call_t(const call_t&)noexcept=default;
+				call_t(call_t&&)noexcept=default;
+			} f =
+			{this,func_p};
+
+			return static_cast<function_t<Func_t>>(f);
 		}
 	};
-
-	typedef comn_ptr_t<library_info_t> library_handle_t;
 }
 
 //file_end
