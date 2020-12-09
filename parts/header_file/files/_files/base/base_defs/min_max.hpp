8d469f727dba25b2e3a57d028ad2a405e01a67a7
ring <54168736+alex0125z@users.noreply.github.com>
2020-12-10 01:01:48 +0800

    Update min_max.hpp

--- parts/header_file/files/_files/base/base_defs/min_max.hpp
+++ parts/header_file/files/_files/base/base_defs/min_max.hpp
@@ -6,6 +6,14 @@
 转载时请在不对此文件做任何修改的同时注明出处
 项目地址：https://github.com/steve02081504/ELC
 */
+/*
+获取几个参数中的最小值或某类型能表示的最小值.
+用法:
+	min.able<T1,T2...> -> bool T...等类型能否获得参数最小值.
+	min.get_limit_able<T> -> bool 能否获得T类型能表示的最小值.
+	min(v1,v2...) -> auto 获取v...的最小值.
+	min(type_info<T>) -> auto 获取T能表示的最小值.
+*/
 constexpr struct min_t{
 	template<class...Args>
 	static constexpr bool able=was_not_an_ill_form(::std::min(declvalue(Args)...));
@@ -22,6 +30,14 @@ constexpr struct min_t{
 		return ::std::numeric_limits<T>::min();
 	}
 }min{};
+/*
+获取几个参数中的最大值或某类型能表示的最大值.
+用法:
+	max.able<T1,T2...> -> bool T...等类型能否获得参数最大值.
+	max.get_limit_able<T> -> bool 能否获得T类型能表示的最大值.
+	max(v1,v2...) -> auto 获取v...的最大值.
+	max(type_info<T>) -> auto 获取T能表示的最大值.
+*/
 constexpr struct max_t{
 	template<class...Args>
 	static constexpr bool able=was_not_an_ill_form(::std::max(declvalue(Args)...));
