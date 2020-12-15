d1259a0770e789e42149da3fc7bd9244c81e93bd
ring <54168736+alex0125z@users.noreply.github.com>
2020-12-15 15:51:46 +0800

    Update zero.hpp

--- parts/header_file/files/_files/base/base_defs/zero.hpp
+++ parts/header_file/files/_files/base/base_defs/zero.hpp
@@ -6,6 +6,9 @@
 转载时请在不对此文件做任何修改的同时注明出处
 项目地址：https://github.com/steve02081504/ELC
 */
+/*
+没什么用的语法糖,等价于字面量0.
+*/
 constexpr struct zero_t{
 	template<class T> requires type_info<decltype(0)>.can_convert_to<T>
 	operator T()const noexcept(type_info<decltype(0)>.can_nothrow_convert_to<T>){return 0;}
