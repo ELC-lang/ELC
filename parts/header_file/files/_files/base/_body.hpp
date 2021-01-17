05217f1839ab0a92ce4ee8194b3be491af321788
NanaKhz <77527350+NanaKhz@users.noreply.github.com>
2021-01-17 21:58:45 +0800

    Update _body.hpp

--- parts/header_file/files/_files/base/_body.hpp
+++ parts/header_file/files/_files/base/_body.hpp
@@ -6,19 +6,19 @@
 转载时请在不对此文件做任何修改的同时注明出处
 项目地址：https://github.com/steve02081504/ELC
 */
-#if defined(ELC_TEST)
-	#error "this part cannot be tested."
+#if defined(ELC_TEST) //查重 对于是否已加载过该hpp文件的判断
+	#error "this part cannot be tested." //中止并提示错误信息
 #endif
 #include "../../../../_share/APIs/alloc.hpp"//using at memory/alloc
 #include "../../../../_share/APIs/die.hpp"//using at memory/gc
 #include "../../../../_share/locale/using.hpp"//strings
-namespace elc::defs{
+namespace elc::defs{ //在elc::defs中定义内容
 	#include "../_share/_defs.hpp"
-	namespace base{
+	namespace base{ //elc::defs::base
 		#include "base_defs/_body.hpp"
 		#include "tools/_body.hpp"
 	}
-	using namespace base;
+	using namespace base; //将定义完的base引入 下同理
 
 	namespace memory{
 		#include "memory/_body.hpp"//依赖list
@@ -35,7 +35,7 @@ namespace elc::defs{
 	}
 	using namespace memory;
 
-	#if defined(ELC_TEST_ON)
+	#if defined(ELC_TEST_ON) //通过检索ELC_TEST_ON是否被def 来定义附加项
 	namespace base_part_test{
 		#include "_test.hpp"
 	}
