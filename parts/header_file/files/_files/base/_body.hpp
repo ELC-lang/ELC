bff71d05cd5141195aca0d1cd07835e366eb9afa
steve green <steve_green@qq.com>
2021-01-17 22:54:48 +0800

    Update _body.hpp

--- parts/header_file/files/_files/base/_body.hpp
+++ parts/header_file/files/_files/base/_body.hpp
@@ -6,19 +6,24 @@
 转载时请在不对此文件做任何修改的同时注明出处
 项目地址：https://github.com/steve02081504/ELC
 */
-#if defined(ELC_TEST) //查重 对于是否已加载过该hpp文件的判断
-	#error "this part cannot be tested." //中止并提示错误信息
+#if defined(ELC_TEST)//若已包含elc/test则报错：其应在此文件后被包含
+	#error "this part cannot be tested."
 #endif
+
+//include APIs：elc中可能根据编译环境而修改的底层函数
 #include "../../../../_share/APIs/alloc.hpp"//using at memory/alloc
 #include "../../../../_share/APIs/die.hpp"//using at memory/gc
+//include locale：自然语言相关的字符串
 #include "../../../../_share/locale/using.hpp"//strings
+
 namespace elc::defs{ //在elc::defs中定义内容
 	#include "../_share/_defs.hpp"
-	namespace base{ //elc::defs::base
+
+	namespace base{
 		#include "base_defs/_body.hpp"
 		#include "tools/_body.hpp"
 	}
-	using namespace base; //将定义完的base引入 下同理
+	using namespace base;//导出内容
 
 	namespace memory{
 		#include "memory/_body.hpp"//依赖list
@@ -33,9 +38,9 @@ namespace elc::defs{ //在elc::defs中定义内容
 	namespace memory::gc_n{
 		#include "memory/gc/_body.hpp"//依赖container
 	}
-	using namespace memory;
+	using namespace memory;//只是强迫症
 
-	#if defined(ELC_TEST_ON) //通过检索ELC_TEST_ON是否被def 来定义附加项
+	#if defined(ELC_TEST_ON)
 	namespace base_part_test{
 		#include "_test.hpp"
 	}
@@ -44,7 +49,7 @@ namespace elc::defs{ //在elc::defs中定义内容
 	#include "../_share/_undefs.hpp"
 }
 namespace elc{
-	#include "_export.hpp"
+	#include "_export.hpp"//导出base承诺于名称空间elc中提供的内容
 }
 
 //file_end
