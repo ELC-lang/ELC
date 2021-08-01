5b1f8d8248812536aa23cc889f37445b630d749c
steve02081504 <steve02081504@EDEN.fukwold>
2021-08-01 10:05:21 +0800

    Update

--- parts/header_file/test/forICE.cpp
+++ parts/header_file/test/forICE.cpp
@@ -1,12 +1,3 @@
-//*
-#define STEVE_H_TEST
-#include <steve.h>
-/*/
-#define al_adapt_steve_h
-#define STEST_ON
-#include <ala>
-//*/
-
 #include <cstdlib>//aligned_alloc:malloc
 namespace std{//cpp20还未实现，占位。
 	template<size_t N,class T>
@@ -16,13 +7,5 @@ namespace std{//cpp20还未实现，占位。
 	#endif
 }
 
-#define ELC_TEST_ON
 #include "../files/base"
 #include "../files/core"
-#include "../files/test"
-
-void ste::stst(){
-	elc::test();
-	elc::test_log_out();
-	elc::test_end();
-}
