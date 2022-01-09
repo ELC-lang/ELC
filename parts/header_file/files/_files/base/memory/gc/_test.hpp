19de1f8ab556e1f6c95abff16234353ac3f12e5d
steve green <steve_green@qq.com>
2022-01-09 13:00:26 +0800

    Update _test.hpp

--- parts/header_file/files/_files/base/memory/gc/_test.hpp
+++ parts/header_file/files/_files/base/memory/gc/_test.hpp
@@ -6,8 +6,8 @@
 转载时请在不对此文件做任何修改的同时注明出处
 项目地址：https://github.com/steve02081504/ELC
 */
-struct gc_tester:can_map_all<gc_tester>,type_info_t<gc_tester>::template_name
-with_common_attribute<can_shrink,count_able,mark_able_for_gc,never_in_array,ref_able>,have_root,build_by_get_only,force_use_default_null_ptr{
+struct gc_tester:type_info_t<gc_tester>::template_name
+with_common_attribute<can_map_all,can_shrink,count_able,mark_able_for_gc,never_in_array,ref_able>,have_root,build_by_get_only,force_use_default_null_ptr{
 	static inline int shrink_time=0;
 	static inline int destroy_time=0;
 	~gc_tester()noexcept{destroy_time++;}
@@ -18,6 +18,7 @@ with_common_attribute<can_shrink,count_able,mark_able_for_gc,never_in_array,ref_
 		shrink_time=destroy_time=0;
 	}
 };
+static_assert(type_info<gc_tester>.base_on<can_map_all<gc_tester>>);
 void map_and_mark_for_gc(gc_tester*a){
 	attribute_ptr_cast<mark_able_for_gc>(a)->mark();
 }
