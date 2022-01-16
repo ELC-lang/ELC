a93247f11ea21bf76dd6d3666068beb486029aec
steve green <steve_green@qq.com>
2022-01-16 21:50:06 +0800

    Create elc-header-file-dev_string.cpp

---
+++ parts/header_file/dev/elc-header-file-dev_string/elc-header-file-dev_string.cpp
@@ -0,0 +1,14 @@
+// elc-header-file-dev_string.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
+//
+/*
+未完成的elc解释器core文件
+由steve02081504与Alex0125设计、编写
+转载时请在不对此文件做任何修改的同时注明出处
+项目地址：https://github.com/steve02081504/ELC
+*/
+#include <iostream>
+
+int main()
+{
+    std::cout << "Hello World!\n";
+}
