5d992d9ee6265db4c8a20b87e60e34692cf22674
steve green <steve_green@qq.com>
2022-01-18 16:35:06 +0800

    sum_string_data_t

--- parts/header_file/dev/elc-header-file-dev_string/elc-header-file-dev_string.cpp
+++ parts/header_file/dev/elc-header-file-dev_string/elc-header-file-dev_string.cpp
@@ -111,14 +111,14 @@ namespace elc::defs{
 
 				end_apply_string_data_t(ptr_t str,string_view_t end):
 					_to_size(str->get_size()),
-					_to(str),
-					_m(note::size((_to_size+end.size())*magic_number::gold_of_resize)),
-					_used_size(end.size())
+					_used_size(end.size()),
+					_m(note::size((_to_size+_used_size)*magic_number::gold_of_resize)),
+					_to(str)
 				{
 					copy_assign[_used_size](note::form(end.begin()),note::to((char_T*)_m));
 				}
 
-				virtual size_t get_size()override{ return _used_size+_to->get_size(); }
+				virtual size_t get_size()override{ return _used_size+_to_size; }
 				virtual void copy_part_data_to(char_T* to,size_t pos,size_t size)override{
 					if(pos+size<_to_size)
 						_to->copy_part_data_to(to,pos,size);
@@ -178,17 +178,19 @@ namespace elc::defs{
 
 				array_t<char_T> _m;//意义为向前延续的数组：当需要向前附加内容时向前拓展以避免重新分配内存
 				ptr_t _to;
+				size_t _to_size;
 				size_t _used_size;
 
 				head_apply_string_data_t(ptr_t str,string_view_t head):
-					_m(note::size(max(str->size()* magic_number::gold ,head.size()))),
-					_to(str),
-					_used_size(head.size())
+					_to_size(str->size()),
+					_used_size(head.size()),
+					_m(note::size((_to_size+_used_size)*magic_number::gold)),
+					_to(str)
 				{
 					copy_assign[_used_size](note::form(head.begin()),note::to((char_T*)_m));
 				}
 
-				virtual size_t get_size()override{ return _used_size+_to->get_size(); }
+				virtual size_t get_size()override{ return _used_size+_to_size; }
 				virtual void copy_part_data_to(char_T* to,size_t pos,size_t size)override{
 					if(pos<_used_size){
 						char_T* head_begin=_m.end()-_used_size;
@@ -238,6 +240,58 @@ namespace elc::defs{
 			base_string_data_t<char_T>::ptr_t base_string_data_t<char_T>::apply_str_to_begin(string_view_t str){
 				return get<head_apply_string_data_t<char_T>>(this,str);
 			}
+
+			template<typename char_T>
+			struct sum_string_data_t:base_string_data_t<char_T>,instance_struct<sum_string_data_t<char_T>>{
+				typedef sum_string_data_t<char_T> this_t;
+				typedef base_string_data_t<char_T> base_t;
+				using base_t::ptr_t;
+				using base_t::string_view_t;
+
+				ptr_t _defore;
+				ptr_t _after;
+				size_t _defore_size;
+				size_t _after_size;
+
+				sum_string_data_t(ptr_t defore,ptr_t after):_defore(defore),_after(after),_defore_size(_defore->get_size()),_after_size(_after->get_size()){}
+
+				virtual size_t get_size()override{ return _defore_size+_after_size; }
+				virtual void copy_part_data_to(char_T* to,size_t pos,size_t size)override{
+					if(pos<_defore_size){
+						auto copy_defore_begin=pos;
+						auto copy_defore_end=min(pos+size,_defore_size);
+						auto copy_defore_size=copy_defore_end-copy_defore_begin;
+						_defore->copy_part_data_to(to,copy_defore_begin,copy_defore_size);
+						if(size!=copy_defore_size){
+							auto copy_after_size=size-copy_defore_size;
+							to+=copy_defore_size;
+							_after->copy_part_data_to(to,0,copy_after_size);
+						}
+					}
+					else
+						_after->copy_part_data_to(to,pos-_defore_size,size);
+				}
+				virtual char_T& arec(size_t index)override{
+					if(index<_defore_size)
+						return _defore->arec(index);
+					else
+						return _after->arec(index-_defore_size);
+				}
+			};
+			template<typename char_T>
+			base_string_data_t<char_T>::ptr_t base_string_data_t<char_T>::apply_str_to_end(ptr_t str){
+				return get<sum_string_data_t<char_T>>(this,str);
+			}
+			template<typename char_T>
+			base_string_data_t<char_T>::ptr_t base_string_data_t<char_T>::apply_str_to_begin(ptr_t str){
+				return get<sum_string_data_t<char_T>>(str,this);
+			}
+		}
+		namespace string_n{
+			template<typename char_T>
+			struct string_t{
+
+			};
 		}
 	}
 }
