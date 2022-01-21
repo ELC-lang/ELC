00ef67c31b6b590cc7aa8075908573440ff10f67
steve green <steve_green@qq.com>
2022-01-21 11:04:36 +0800

    Update elc-header-file-dev_string.cpp

--- parts/header_file/dev/elc-header-file-dev_string/elc-header-file-dev_string.cpp
+++ parts/header_file/dev/elc-header-file-dev_string/elc-header-file-dev_string.cpp
@@ -753,7 +753,7 @@ namespace elc::defs{
 		private:
 			struct iterator_base_t{
 				string_t* _to;
-				ptrdiff_t _index;
+				ptrdiff_t _index;//rend: -1
 
 				[[nodiscard]]constexpr iterator_base_t get_before()const noexcept{ return {_to,_index-1}; }
 				[[nodiscard]]constexpr iterator_base_t get_next()const noexcept{ return {_to,_index+1}; }
@@ -767,8 +767,8 @@ namespace elc::defs{
 			typedef iterator_t<char_T,iterator_base_t>iterator;
 			typedef const_iterator_t<char_T,const iterator_base_t>const_iterator;
 
-			iterator get_iterator_at(size_t index){ return get_iterator_data_at(index); }
-			const_iterator get_iterator_at(size_t index)const{ return get_iterator_data_at(index); }
+			iterator get_iterator_at(ptrdiff_t index){ return get_iterator_data_at(index); }
+			const_iterator get_iterator_at(ptrdiff_t index)const{ return get_iterator_data_at(index); }
 			iterator begin(){ return get_iterator_at(0); }
 			const_iterator begin()const{ return get_iterator_at(0); }
 			const_iterator cbegin()const{ return begin(); }
@@ -779,8 +779,8 @@ namespace elc::defs{
 			typedef reverse_iterator_t<char_T,iterator_base_t>reverse_iterator;
 			typedef reverse_const_iterator_t<char_T,const iterator_base_t>reverse_const_iterator;
 
-			reverse_iterator get_reverse_iterator_at(size_t index){ return get_iterator_data_at(index); }
-			reverse_const_iterator get_reverse_iterator_at(size_t index)const{ return get_iterator_data_at(index); }
+			reverse_iterator get_reverse_iterator_at(ptrdiff_t index){ return get_iterator_data_at(index); }
+			reverse_const_iterator get_reverse_iterator_at(ptrdiff_t index)const{ return get_iterator_data_at(index); }
 			reverse_iterator rbegin(){ return get_reverse_iterator_at(size()-1); }
 			reverse_const_iterator rbegin()const{ return get_reverse_iterator_at(size()-1); }
 			reverse_const_iterator rcbegin()const{ return rbegin(); }
