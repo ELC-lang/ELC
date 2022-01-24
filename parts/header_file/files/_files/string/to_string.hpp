3cf2e6b89e8c47495c47fa3dae427ec5608febdf
steve green <steve_green@qq.com>
2022-01-25 00:21:44 +0800

    Update to_string.hpp

--- parts/header_file/files/_files/string/to_string.hpp
+++ parts/header_file/files/_files/string/to_string.hpp
@@ -9,62 +9,66 @@
 //UF
 namespace to_string_n{
 	string to_string(nothing){
-		return "nothing"_elc_string;
+		return es"nothing"_elc_string;
 	}
 	template<typename T>
-	string num_base(T num,size_t radix,const string&radix_table){
-		if constexpr(::std::is_floating_point_v<T>)
-			num=num%1;
+	string num_base(T num,size_t radix,const string radix_table){
 		string aret;
 		do{//do while，在num为0时也有返值
-			auto first_char_index=num%radix;
+			T first_char_index{};
+			if constexpr(::std::is_floating_point_v<T>)
+				first_char_index= ::std::fmod(num,(T)radix);
+			else
+				first_char_index= num%radix;
 			if constexpr(::std::is_floating_point_v<T>)
 				num-=first_char_index;
-			num/=radix;
-			aret.push_front(radix_table[first_char_index]);
+			num/=(T)radix;
+			aret.push_front(radix_table[(size_t)first_char_index]);
 		}
 		while(num);
 		return aret;
 	}
 	template<typename T>
-	string num_base_mantissa(T num,size_t radix,const string&radix_table){
+	string num_base_mantissa(T num,size_t radix,const string radix_table){
 		string aret;
 		while(num){
-			auto first_char_index=num%1;
-			num-=first_char_index;
+			auto first_char_index=::std::modf(num,&num);
 			num*=radix;
-			aret+=radix_table[first_char_index];
+			aret+=radix_table[(size_t)first_char_index];
 		}
 		return aret;
 	}
 	template<typename T> requires ::std::is_arithmetic_v<T>
-	string to_string(T num,size_t radix=10,const string&radix_table="0123456789abcdefghigklmnopqrstuvwxyz"_elc_string){
+	string to_string(T num,size_t radix=10,const string radix_table=es"0123456789abcdefghigklmnopqrstuvwxyz"_elc_string){
 		if constexpr(::std::is_floating_point_v<T>){//float特殊值检查
 			if constexpr(::std::numeric_limits<T>::has_signaling_NaN){
-				if(num==::std::numeric_limits<T>::signaling_NaN)
-					return "signaling_NaN"_elc_string;
+				if(num==::std::numeric_limits<T>::signaling_NaN())
+					return es"signaling_NaN"_elc_string;
 			}
 			if constexpr(::std::numeric_limits<T>::has_quiet_NaN){
-				if(num==::std::numeric_limits<T>::quiet_NaN)
-					return "quiet_NaN"_elc_string;
+				if(num==::std::numeric_limits<T>::quiet_NaN())
+					return es"quiet_NaN"_elc_string;
 			}
 			if constexpr(::std::numeric_limits<T>::has_infinity){
-				if(num==::std::numeric_limits<T>::infinity)
-					return "infinity"_elc_string;
-				if(num==-::std::numeric_limits<T>::infinity)
-					return "-infinity"_elc_string;
+				if(num==::std::numeric_limits<T>::infinity())
+					return es"infinity"_elc_string;
+				if(num==-::std::numeric_limits<T>::infinity())
+					return es"-infinity"_elc_string;
 			}
 		}
 		string aret;
 		if constexpr(!::std::is_unsigned_v<T>)
-			if(num < 0)
-				aret='-';
-		aret+=num_base(num,radix,radix_table);
+			if(num < 0){
+				aret=ec('-');
+				num=-num;
+			}
 		if constexpr(::std::is_floating_point_v<T>){
-			if(auto t=num%1){
-				aret='.'+num_base_mantissa(t,radix,radix_table)
+			T num_fractional=::std::modf(num,&num); 
+			if(num_fractional){
+				aret+=ec('.')+num_base_mantissa(num_fractional,radix,radix_table);
 			}
 		}
+		aret.push_front(num_base(num,radix,radix_table));
 		return aret;
 	}
 }
