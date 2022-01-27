477a40dc6acb1cf8148432a5bb0d5c82437a7a50
steve green <steve_green@qq.com>
2022-01-27 15:34:12 +0800

    艹

--- parts/header_file/dev/elc-header-file-dev_literal/elc-header-file-dev_literal.cpp
+++ parts/header_file/dev/elc-header-file-dev_literal/elc-header-file-dev_literal.cpp
@@ -1,6 +1,6 @@
 // elc-header-file-dev_literal.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
 /*
-未完成的elc解释器core文件
+未完成的elc解释器dev文件
 由steve02081504与Alex0125设计、编写
 转载时请在不对此文件做任何修改的同时注明出处
 项目地址：https://github.com/steve02081504/ELC
