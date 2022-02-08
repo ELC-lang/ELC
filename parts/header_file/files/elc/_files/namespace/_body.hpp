5ee932c2258ff556e59275d8487b220971eea328
steve green <steve_green@qq.com>
2022-02-08 20:42:52 +0800

    namespace

---
+++ parts/header_file/files/elc/_files/namespace/_body.hpp
@@ -0,0 +1,28 @@
+//_body.hpp
+//at namespace ::
+/*
+未完成的elc解释器namespace文件
+由steve02081504与Alex0125设计、编写
+转载时请在不对此文件做任何修改的同时注明出处
+项目地址：https://github.com/steve02081504/ELC
+*/
+#if defined(ELC_TEST)
+	#error "this part cannot be tested."
+#endif
+namespace elc::defs{
+	#include "../_share/_defs.hpp"
+
+	#if defined(ELC_TEST_ON)
+	namespace namespace_part_test{
+		#include "_test.hpp"
+	}
+	#endif
+
+	#include "../_share/_undefs.hpp"
+}
+namespace elc{
+	#include "_export.hpp"
+}
+
+//file_end
+
---
+++ parts/header_file/files/elc/_files/namespace/_test.hpp
@@ -0,0 +1,17 @@
+//_test.hpp
+/*
+未完成的elc解释器namespace文件
+由steve02081504与Alex0125设计、编写
+转载时请在不对此文件做任何修改的同时注明出处
+项目地址：https://github.com/steve02081504/ELC
+*/
+inline void test(){
+	ELC_TEST_EVENTNAME("namespace部分测试");
+}
+inline void test_log_out(){
+}
+inline void test_end{
+}
+
+//file_end
+
