d720fe337f8eae89935e1720c390606405f04d09
Alex0125 <alex-jas-de-brad@qq.com>
2022-02-26 21:21:01 +0800

    fix.

--- parts/_share/APIs/str_code_convert.hpp
+++ parts/_share/APIs/str_code_convert.hpp
@@ -21,8 +21,8 @@ elc依赖的基础函数.
 		inline string_t<char> to_char_str(string_view_t<char_t> a)noexcept{
 			string_t<char>aret;
 			::std::mbstate_t stat{};
-			char*old_lc;
-			if((old_lc=::std::setlocale(LC_CTYPE,"en_US.utf8"))==nullptr)
+			string_t old_lc=::std::setlocale(LC_CTYPE,nullptr);
+			if(::std::setlocale(LC_CTYPE,"en_US.utf8")==nullptr)
 				die_with(locale::str::setlocale_error);
 			#if defined(_MSC_VER)
 				#pragma warning(push)
@@ -39,14 +39,14 @@ elc依赖的基础函数.
 					die_with(locale::str::code_convert_error);
 				aret+=string_view_t<char>{(char*)r,note::size((size_t)s)};
 			}
-			::std::setlocale(LC_CTYPE,old_lc);
+			::std::setlocale(LC_CTYPE,old_lc.c_str());
 			return aret;
 		}
 		inline string_t<char_t> to_char_t_str(string_view_t<char> a)noexcept{
 			string_t<char_t>aret;
 			::std::mbstate_t stat{};
-			char*old_lc;
-			if((old_lc=::std::setlocale(LC_CTYPE,"en_US.utf8"))==nullptr)
+			string_t old_lc=::std::setlocale(LC_CTYPE,nullptr);
+			if(::std::setlocale(LC_CTYPE,"en_US.utf8")==nullptr)
 				die_with(locale::str::setlocale_error);
 			#if defined(_MSC_VER)
 				#pragma warning(push)
@@ -65,7 +65,7 @@ elc依赖的基础函数.
 				i+=s;
 				aret+=c;
 			}
-			::std::setlocale(LC_CTYPE,old_lc);
+			::std::setlocale(LC_CTYPE,old_lc.c_str());
 			return aret;
 		}
 		inline string_t<char> to_char_str(const char_t*a)noexcept{
