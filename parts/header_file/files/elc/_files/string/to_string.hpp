d6eb81fec3a56552c5035d1c8ce236deb48dad14
steve02081504 <steve02081504@EDEN.fukwold>
2022-09-02 17:39:41 +0800

    Update to_string.hpp

--- parts/header_file/files/elc/_files/string/to_string.hpp
+++ parts/header_file/files/elc/_files/string/to_string.hpp
@@ -70,13 +70,13 @@ namespace to_string_n{
 				aret=ec('-');
 				unum=UT(-num);
 			}
-		if constexpr(::std::is_floating_point_v<T>){
-			UT num_fractional=::std::modf(unum,&unum);
-			if(num_fractional){
+		UT num_fractional{};
+		if constexpr(::std::is_floating_point_v<T>)
+			num_fractional=::std::modf(unum,&unum);
+		aret+=num_base(unum,radix,radix_table);
+		if constexpr(::std::is_floating_point_v<T>)
+			if(num_fractional)
 				aret+=ec('.')+num_base_mantissa(num_fractional,radix,radix_table);
-			}
-		}
-		aret.push_front(num_base(unum,radix,radix_table));
 		return aret;
 	}
 	template<typename T> requires ::std::is_arithmetic_v<T>
@@ -140,10 +140,11 @@ namespace from_string_get_n{
 		}()) UT;
 		UT unum{};
 		bool is_negative=false;
-		if(str[0]==ec('-')){
-			is_negative=true;
-			str.pop_front();
-		}
+		if constexpr(!::std::is_unsigned_v<T>)
+			if(str[0]==ec('-')){
+				is_negative=true;
+				str.pop_front();
+			}
 		if constexpr(::std::is_floating_point_v<T>) {
 			const size_t dot_pos=str.find(ec('.'));
 			if(dot_pos!=string::npos){
@@ -151,11 +152,8 @@ namespace from_string_get_n{
 				str=str.substr(0,dot_pos);
 				unum+=num_base_mantissa<UT>(mantissa_str,radix,radix_table);
 			}
-			unum+=num_base<UT>(str,radix,radix_table);
 		}
-		else{
-			unum=num_base<UT>(str,radix,radix_table);
-		}
+		unum+=num_base<UT>(str,radix,radix_table);
 		if(is_negative)
 			return T(T{}-unum);
 		else
