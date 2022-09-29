592773238d7d5db110d1e6acb0f076bbce3be660
steve02081504 <steve02081504@EDEN.fukwold>
2022-09-29 19:02:37 +0800

    Update str_code_convert.hpp

--- parts/_share/APIs/str_code_convert.hpp
+++ parts/_share/APIs/str_code_convert.hpp
@@ -12,8 +12,12 @@ elc依赖的基础函数.
 */
 #if !defined(ELC_APIS_str_code_convert)
 	#define ELC_APIS_str_code_convert
+	#include "_tools/decl_system_type.hpp"
 	#include <cuchar>
 	#include <clocale>
+	#if SYSTEM_TYPE == windows
+		#include <Windows.h>//<stringapiset.h>
+	#endif
 	#include "../default_data_type.hpp"
 	#include "../../header_file/files/elc/string"
 	namespace elc::APIs::str_code_convert{
@@ -38,10 +42,38 @@ elc依赖的基础函数.
 				aret+=string_view_t<char>{(char*)r,note::size((size_t)s)};
 			}
 			::std::setlocale(LC_CTYPE,old_lc.c_str());
+			#if SYSTEM_TYPE == windows
+			{
+				array_t<wchar_t>aret_w;
+				auto len=MultiByteToWideChar(CP_UTF8, 0, aret.data(), aret.size(), NULL, 0);
+				aret_w.resize(len);
+				if(!MultiByteToWideChar(CP_UTF8, 0, aret.data(), aret.size(), aret_w.begin(), aret_w.size()))
+					die_with(locale::str::code_convert_error);
+				len = WideCharToMultiByte(CP_ACP, 0, aret_w.begin(), aret_w.size(), NULL, 0, NULL, NULL);
+				aret.resize(len);
+				if(!WideCharToMultiByte(CP_ACP, 0, aret_w.begin(), aret_w.size(), aret.writeable_data(), aret.size(), NULL, NULL))
+					die_with(locale::str::code_convert_error);
+			}
+			#endif
 			return aret;
 		}
 		[[nodiscard]]inline string_t<char_t> to_char_t_str(string_view_t<char> a)noexcept{
 			string_t<char_t>aret;
+			#if SYSTEM_TYPE == windows
+			string_t<char>arg_base;
+			{
+				array_t<wchar_t>aret_w;
+				auto len=MultiByteToWideChar(CP_ACP, 0, a.data(), a.size(), NULL, 0);
+				aret_w.resize(len);
+				if(!MultiByteToWideChar(CP_ACP, 0, a.data(), a.size(), aret_w.begin(), aret_w.size()))
+					die_with(locale::str::code_convert_error);
+				len = WideCharToMultiByte(CP_UTF8, 0, aret_w.begin(), aret_w.size(), NULL, 0, NULL, NULL);
+				arg_base.resize(len);
+				if(!WideCharToMultiByte(CP_UTF8, 0, aret_w.begin(), aret_w.size(), arg_base.writeable_data(), arg_base.size(), NULL, NULL))
+					die_with(locale::str::code_convert_error);
+				a = arg_base;
+			}
+			#endif
 			::std::mbstate_t stat{};
 			string_t old_lc=::std::setlocale(LC_CTYPE,nullptr);
 			if(::std::setlocale(LC_CTYPE,"en_US.utf8")==nullptr)
@@ -67,6 +99,8 @@ elc依赖的基础函数.
 	namespace elc::defs{
 		using namespace elc::APIs::str_code_convert;
 	}
+	//
+	#include "_tools/undef_decl_system_type.hpp"
 #endif
 
 //file_end
