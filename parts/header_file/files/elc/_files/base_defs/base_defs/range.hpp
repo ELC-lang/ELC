a60bd86fd62f3c8991d4ad3bcb91f12f9a2b30ab
steve02081504 <steve02081504@EDEN.fukwold>
2022-11-27 00:57:10 +0800

    BMH2 -> sunday
    Co-Authored-By: Baobaobear <53596740+Baobaobear@users.noreply.github.com>
    Co-Authored-By: ring <54168736+alex0125z@users.noreply.github.com>

--- parts/header_file/files/elc/_files/base_defs/base_defs/range.hpp
+++ parts/header_file/files/elc/_files/base_defs/base_defs/range.hpp
@@ -54,7 +54,7 @@ namespace range_n{
 		return cast_to_data(pattern)>=range.begin() && cast_to_data(pattern)<range.end();
 	}
 	//  match_pattern
-	/// BMH2改算法表头，用以实施头起始的快速子串匹配算法
+	/// sunday算法表头，用以实施头起始的快速子串匹配算法
 	/// 大部分情况下应当编译时预构建或对运行时生成的实例进行缓存
 	template<typename T>
 	struct match_pattern{
@@ -62,8 +62,6 @@ namespace range_n{
 		typedef unsigned char index_type;
 
 		size_t skip_table[number_of_possible_values_per<index_type>]{};
-		size_t radical_skip_table[number_of_possible_values_per<index_type>]{};
-		index_type pre_index_table[number_of_possible_values_per<index_type>]{};
 
 		static constexpr index_type get_index_of(T&ch)noexcept{
 			return index_type(hash(ch) % number_of_possible_values_per<index_type>);
@@ -73,43 +71,32 @@ namespace range_n{
 			if(!m)
 				return;
 			for(size_t i=0;i<number_of_possible_values_per<index_type>;i++)
-				skip_table[i]=radical_skip_table[i]=m;
-			skip_table[get_index_of(pattern[0])]=radical_skip_table[get_index_of(pattern[0])]=m-1;//单独处理pattern[0]的情况
-			for(size_t i=1;i<m-1;i++){
-				index_type index=get_index_of(pattern[i]);//radical_skip_table[index]表示pattern中倒数第二次出现的index到pattern末尾的距离
-				radical_skip_table[index]=skip_table[index];//当index在pattern中出现0次或1次时，radical_skip_table[index]等于模式串长度m
-				skip_table[index]=m-i-1;//skip数组的定义与BMH算法相同
-				pre_index_table[index]=get_index_of(pattern[i-1]);
-			}
+				skip_table[i]=m+1;
+			for(size_t i=0;i<m;i++)
+				skip_table[get_index_of(pattern[i])]=m-i;
 		}
 		constexpr match_pattern(const array_like_view_t<T>&pattern)noexcept:_pattern(pattern){
 			build_table(_pattern);
 		}
 		[[nodiscard]]constexpr T* match(const array_like_view_t<T>&range)const noexcept{
-			size_t m = _pattern.size();
-			if(!m)
-				return range.begin();
-			size_t n= range.size();
-			size_t i= m-1;
-			while(i<n){
-				size_t k=i;
-				ptrdiff_t j=m-1;//k记录text中每次从右至左开始比较的起始位置
-				while((j>=0)&&(_pattern[j]==range[i])){
-					i--;j--;
-				}
-				if(j==-1)
-					return addressof(range[i+1]);//在range[i+1]处匹配成功
-				index_type index_k=get_index_of(range[k]);
-				if(get_index_of(range[k-1])!=pre_index_table[index_k])
-					i=k+radical_skip_table[index_k];//采用激进策略移动文本指针
-				else
-					i=k+skip_table[index_k];
+			size_t off_set=0;
+			size_t matching_off_set=1;
+
+			floop{
+				matching_off_set=1;
+				while(_pattern.end()[0-matching_off_set]==range.begin()[off_set+_pattern.size()-matching_off_set])
+					if(matching_off_set==_pattern.size())
+						return addressof(range.begin()[off_set]);
+					else
+						matching_off_set++;
+				if(off_set+_pattern.size() >= range.size())
+					return nullptr;
+				off_set+=skip_table[get_index_of(range.begin()[off_set+_pattern.size()])];
 			}
-			return nullptr;//匹配失败
 		}
 	};
 	//  reverse_match_pattern
-	/// 反向BMH2改算法表头，用以实施尾起始的快速子串匹配算法
+	/// 反向Sunday算法表头，用以实施尾起始的快速子串匹配算法
 	/// 大部分情况下应当编译时预构建或对运行时生成的实例进行缓存
 	template<typename T>
 	struct reverse_match_pattern{
@@ -117,8 +104,6 @@ namespace range_n{
 		typedef unsigned char index_type;
 
 		size_t skip_table[number_of_possible_values_per<index_type>]{};
-		size_t radical_skip_table[number_of_possible_values_per<index_type>]{};
-		index_type pre_index_table[number_of_possible_values_per<index_type>]{};
 
 		static constexpr index_type get_index_of(T&ch)noexcept{
 			return index_type(hash(ch) % number_of_possible_values_per<index_type>);
@@ -128,39 +113,29 @@ namespace range_n{
 			if(!m)
 				return;
 			for(size_t i=0;i<number_of_possible_values_per<index_type>;i++)
-				skip_table[i]=radical_skip_table[i]=m;
-			skip_table[get_index_of(pattern[m-1])]=radical_skip_table[get_index_of(pattern[m-1])]=m-1;//单独处理pattern[m-1]的情况
-			for(ptrdiff_t i=m-2;i>=0;i--){
-				index_type index=get_index_of(pattern[i]);//radical_skip_table[index]表示pattern中倒数第二次出现的index到pattern末尾的距离
-				radical_skip_table[index]=skip_table[index];//当index在pattern中出现0次或1次时，radical_skip_table[index]等于模式串长度m
-				skip_table[index]=i;//skip数组的定义与BMH算法相同
-				pre_index_table[index]=get_index_of(pattern[i+1]);
-			}
+				skip_table[i]=m+1;
+			for(size_t i=0;i<m-1;i++)
+				skip_table[get_index_of(pattern[i])]=i;
 		}
 		constexpr reverse_match_pattern(const array_like_view_t<T>&pattern)noexcept:_pattern(pattern){
 			build_table(_pattern);
 		}
 		[[nodiscard]]constexpr T* match(const array_like_view_t<T>&range)const noexcept{
-			size_t m=_pattern.size();
-			if(!m)
-				return range.begin();
-			size_t n= range.size();
-			ptrdiff_t i= n-m;
-			while(i>=0){
-				size_t k=i;
-				size_t j=0;//k记录text中每次从右至左开始比较的起始位置
-				while((j<=m-1)&&(_pattern[j]==range[i])){
-					i++;j++;
-				}
-				if(j==m)
-					return addressof(range[k]);//在range[k]处匹配成功
-				index_type index_k=get_index_of(range[k]);
-				if(get_index_of(range[k+1])!=pre_index_table[index_k])
-					i=k-radical_skip_table[index_k];//采用激进策略移动文本指针
-				else
-					i=k-skip_table[index_k];
+			size_t off_set=0;
+			size_t matching_off_set=0;
+
+			floop{
+				matching_off_set=0;
+				while(_pattern.begin()[matching_off_set]==range.end()[0-(off_set+_pattern.size()-matching_off_set)])
+					if(matching_off_set==_pattern.size()-1)
+						return addressof(range.end()[0-(off_set+_pattern.size())]);
+					else
+						matching_off_set++;
+				if(off_set+_pattern.size() >= range.size())
+					return nullptr;
+				off_set+=skip_table[get_index_of(range.end()[0-(off_set+_pattern.size()+1)])];
+				off_set++;
 			}
-			return nullptr;//匹配失败
 		}
 	};
 	suppress_msvc_warning(26475)//强制转换警告diss
@@ -203,32 +178,12 @@ namespace range_n{
 		else
 			return npos;
 	}
-	//  数据串匹配by steve02081504.
 	/// 若成功找到匹配的数据串，返回其开头，若未找到，返回nullptr
 	template<typename T>
-	[[nodiscard]]constexpr T* in_range(const array_like_view_t<T>&pattern,const array_like_view_t<T>&range){
-		if(range.size() >= 512 && pattern.size() >= 11){
-			match_pattern tmp=pattern;
-			return tmp.match(range);
-		}
-
-		size_t off_set=0;
-		size_t matching_off_set=1;
-
-		floop{
-			matching_off_set=1;
-			while(pattern.end()[0-matching_off_set]==range.begin()[off_set+pattern.size()-matching_off_set])
-				if(matching_off_set==pattern.size())
-					return addressof(range.begin()[off_set]);
-				else
-					matching_off_set++;
-			if(off_set+pattern.size() >= range.size())
-				return nullptr;
-			auto tmp = in_range(range.begin()[off_set+pattern.size()], pattern);
-			if(!tmp)
-				tmp=pattern.begin();
-			off_set+=pattern.end()-tmp;
-		}
+	[[nodiscard,deprecated("cache match_pattern and don't waste it")]]
+	constexpr T* in_range(const array_like_view_t<T>&pattern,const array_like_view_t<T>&range){
+		match_pattern tmp=pattern;
+		return tmp.match(range);
 	}
 	/// 若成功找到匹配的数据串，返回其距离开头的步数，若未找到，返回npos
 	template<typename T>
@@ -242,30 +197,10 @@ namespace range_n{
 	//  反向数据串匹配by steve02081504.
 	/// 若成功找到匹配的数据串，返回其开头，若未找到，返回nullptr
 	template<typename T>
-	[[nodiscard]]constexpr T* in_range_but_reverse(const array_like_view_t<T>&pattern,const array_like_view_t<T>&range){
-		if(range.size() >= 512 && pattern.size() >= 11){
-			reverse_match_pattern tmp = pattern;
-			return tmp.match(range);
-		}
-
-		size_t off_set=0;
-		size_t matching_off_set=0;
-
-		floop{
-			matching_off_set=0;
-			while(pattern.begin()[matching_off_set]==range.end()[0-(off_set+pattern.size()-matching_off_set)])
-				if(matching_off_set==pattern.size()-1)
-					return addressof(range.end()[0-(off_set+pattern.size())]);
-				else
-					matching_off_set++;
-			if(off_set+pattern.size() >= range.size())
-				return nullptr;
-			auto tmp = in_range_but_reverse(range.end()[0-(off_set+pattern.size()+1)], pattern);
-			if(!tmp)
-				tmp=pattern.end()-1;
-			off_set+=tmp-pattern.begin();
-			off_set+=1;
-		}
+	[[nodiscard,deprecated("cache match_pattern and don't waste it")]]
+	constexpr T* in_range_but_reverse(const array_like_view_t<T>&pattern,const array_like_view_t<T>&range){
+		reverse_match_pattern tmp = pattern;
+		return tmp.match(range);
 	}
 	/// 若成功找到匹配的数据串，返回其距离开头的步数，若未找到，返回npos
 	template<typename T>
