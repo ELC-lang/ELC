c7e5de8748fb88d61c2be6576695ce50f280a70b
steve02081504 <steve02081504@EDEN.fukwold>
2022-11-29 20:19:16 +0800

    refix

--- parts/_share/basic_environment/_body.hpp
+++ parts/_share/basic_environment/_body.hpp
@@ -22,21 +22,49 @@ namespace elc::defs{
 		}());
 		//任意类型转算数类型
 		template<class T>
-		force_inline constexpr bool is_arithmetic_convertible_helper()noexcept{
-			size_t muti_convertible_count=0;
-			#define TYPE_MAPPER(type) if constexpr(std::is_convertible_v<T,type>)muti_convertible_count++;
+		constexpr bool is_arithmetic_convertible_able_helper()noexcept{
+			#define TYPE_MAPPER(type) if constexpr(::std::is_convertible_v<T,type>)return true;else
 			#include "./arithmetic_mapper/all_mapper.hpp"
 			#undef TYPE_MAPPER
-			return muti_convertible_count==1;
+			return false;
 		}
 		template<class T>
-		constexpr bool is_arithmetic_convertible= is_arithmetic_convertible_helper<T>();
-		template<class T> requires(is_arithmetic_convertible<T>)
-		force_inline constexpr auto to_arithmetic(T&&v)noexcept{
+		force_inline constexpr auto to_arithmetic_base(T&&v)noexcept{
 			#define TYPE_MAPPER(type) if constexpr(::std::is_convertible_v<T,type>)return(type)v;else
 			#include "./arithmetic_mapper/all_mapper.hpp"
 			#undef TYPE_MAPPER
 			{}
+		}
+		//这里的定义不能使用basedefs中的type_info，所以得重新造一个小轮子
+		struct type_uniquer_t{
+			constexpr bool operator==(const type_uniquer_t&other)const noexcept{return this==&other;}
+		};
+		template<class T>
+		constexpr type_uniquer_t type_uniquer{};
+		template<class T>
+		constexpr bool is_arithmetic_convertible_unique_helper()noexcept{
+			typedef decltype(to_arithmetic_base(declvalue(T))) my_type;
+			size_t muti_convertible_count=0;
+			#define TYPE_MAPPER(type) \
+			if constexpr(type_uniquer<my_type>!=type_uniquer<type>)\
+				muti_convertible_count++;
+			#include "./arithmetic_mapper/all_mapper.hpp"
+			#undef TYPE_MAPPER
+			return muti_convertible_count==0;
+		}
+		template<class T>
+		constexpr bool is_arithmetic_convertible_helper_final()noexcept{
+			if constexpr(is_arithmetic_convertible_able_helper<T>())
+				return is_arithmetic_convertible_unique_helper<T>();
+			else
+				return false;
+		}
+		template<class T>
+		constexpr bool is_arithmetic_convertible= is_arithmetic_convertible_helper_final<T>();
+		//算数类型转任意类型
+		template<class T> requires(is_arithmetic_convertible<T>)
+		force_inline constexpr auto to_arithmetic(T&&v)noexcept{
+			return to_arithmetic_base(::std::forward<T>(v));
 		}
 	}
 	using basic_environment::BIT_POSSIBILITY;
