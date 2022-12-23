b9b44726c087dc4dea7e5642b98eb8d5fcc9c2e8
steve02081504 <steve02081504@EDEN.fukwold>
2022-12-23 11:33:49 +0800

    Create _test.hpp

---
+++ parts/header_file/files/elc/_files/base/big_number_types/_test.hpp
@@ -0,0 +1,18 @@
+//_test.hpp
+//at namespace elc::defs::big_number_types
+/*
+未完成的elc解释器base文件
+由steve02081504与Alex0125设计、编写
+转载时请在不对此文件做任何修改的同时注明出处
+项目地址：https://github.com/steve02081504/ELC
+*/
+inline void test(){
+	ELC_TEST_EVENTNAME("big_number_types部分测试");
+}
+inline void test_log_out(){
+}
+inline void test_end(){
+}
+
+//file_end
+
